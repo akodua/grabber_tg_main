02964fc8bda0f780a88a5f51d5e48dc391ece13d
xabz <117483033+akodua@users.noreply.github.com>
2024-11-25 19:29:00 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+api_id = 1111  # Ваш API ID для Telethon
+api_hash = '1111'  # Ваш API Hash для Telethon
+bot_token = '1111'  # Токен вашого бота
+my_id = 1111  # Ваш ID користувача для отримання повідомлень
+proxy_url = None  # Проксі, якщо використовуєте
